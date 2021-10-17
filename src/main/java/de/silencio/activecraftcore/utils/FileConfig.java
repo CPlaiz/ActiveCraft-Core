6292eb290f90fbdc87f45044f12e4b2022644573
CPlaiz <treesarne@gmail.com>
2021-10-17 11:14:29 +0200

    Removed annoying debug stuff

--- src/main/java/de/silencio/activecraftcore/utils/FileConfig.java
+++ src/main/java/de/silencio/activecraftcore/utils/FileConfig.java
@@ -3,6 +3,7 @@ package de.silencio.activecraftcore.utils;
 import de.silencio.activecraftcore.Main;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +22,10 @@ public class FileConfig extends YamlConfiguration {
         } catch (InvalidConfigurationException | IOException ex) {
             if (!(ex instanceof FileNotFoundException)) ex.printStackTrace();
         }
+    }
+    
+    public FileConfig(String filename, Plugin plugin) {
+        this(plugin.getDataFolder().getName(), filename);
     }
 
     public FileConfig(String filename) {
